c42d474243fcbfc7086577dc8444d3b6720e9a3b
deyu <deyu.zhang@tendcloud.com>
2023-06-07 00:27:47 +0800

    增加插件

--- Java/TestClass.java
+++ Java/TestClass.java
@@ -2,6 +2,6 @@ package Java;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello World! 111");
+        System.out.println("Hello World!");
     }
 }
